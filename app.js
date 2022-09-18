6caff40389ad9a15a0d5caf4e2aa479c7c3c55c7
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-18 17:25:11 +0530

    1.03-Added ejs file.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express(); // creating a simple constant variable for the express server to run with more english meaning.
 
+app.use('view engine', 'ejs');
+
 const PORT = 3000 || process.env.PORT; // 3000 port runs always on local host but process.env.port will get any free port on internet server and then will run on that port.
 
 app.get("/",function(req,res){
