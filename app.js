40dc00f34bb115bfe3b753a4b2f0a37c2fc2be31
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-18 17:02:11 +0530

    1.01-Added basic express structure.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express(); // creating a simple constant variable for the express server to run with more english meaning.
+
+const PORT = 3000 || process.env.PORT; // 3000 port runs always on local host but process.env.port will get any free port on internet server and then will run on that port.
+
+app.get("/",function(req,res){
+    res.send("<h1>Hello bro</h1>");
+})
+
+
+app.listen(PORT,function(){
+    console.log("server started on port "+PORT+" bro!");
+});
