68f9cb30f6518681195c5cb9506a7294471eea88
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-19 15:15:16 +0530

    1.04-Added get fuction in app.js which passes the day to list.ejs file via dayofweek variable.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+// jshint esversion 6
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = 3000 || process.env.PORT; // 3000 port runs always on local host but process.env.port will get any free port on internet server and then will run on that port.
 
 const app = express(); // creating a simple constant variable for the express server to run with more english meaning.
 
-app.use('view engine', 'ejs');
+app.set("view engine", "ejs"); // this command sets the default view engine to ejs so that it can fetch the list.ejs file from the views folder. list.ejs is just an ordinary filename you can use whatever name you want, most people use index.ejs.
 
-const PORT = 3000 || process.env.PORT; // 3000 port runs always on local host but process.env.port will get any free port on internet server and then will run on that port.
 
-app.get("/",function(req,res){
-    res.send("<h1>Hello bro</h1>");
-})
+app.get("/", function(req,res){
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    res.render("list", {dayofweek:week[currentDay]});
+});
 
 
 app.listen(PORT,function(){
