f8316f5219c10e741e929d4b72394a5c60b5f18d
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-24 17:01:18 +0530

    2.7- Added comments for a little bit better understanding.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { text } = require('body-parser');
 const date = require(__dirname+'/date.js');
 const PORT = process.env.PORT || 3000; // 3000 port runs always on local host but process.env.port will get any free port on internet server and then will run on that port.
 
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public")); //this is used to serve static files like styles, images, other assets which are kept in a folder  called as public. 
 
 app.set("view engine", "ejs"); // this command sets the default view engine to ejs so that it can fetch the list.ejs file from the views folder. list.ejs is just an ordinary filename you can use whatever name you want, most people use index.ejs.
+
+const day=date.getDay();
+
 
 const URL='mongodb://0.0.0.0:27017/todolistDB';
 mongoose.connect(URL,function(err){
@@ -23,12 +27,17 @@ mongoose.connect(URL,function(err){
     }
 });
 
+// creating schema for items(blueprint)
 const itemsSchema = new mongoose.Schema({
     name:String,
 });
 
+// creating model using the itemsSchema and the items collection. 
+// ps: always remember the first parameter in the model generation is singular name of the collection
+//  ex: if the name of the collection is people then the first parameter should be Person(Singular Noun).
 const Item = new mongoose.model("Item",itemsSchema);
 
+// creating temporary documents(records) and adding them into the default_Array.
 const item1 = new Item({
     name:"Sternritters of Bleach"
 });
@@ -41,14 +50,12 @@ const item3 = new Item({
 
 const default_Array = [item1,item2,item3];
 
-// let task_array=[];
-// let work_array=[];
-// let newTask="";
-const day=date.getDay();
 
 
 
-
+//when a get request is sent on this route then it first fetches all the documents available in the Items collection of tolistDB in mongodb
+// and if the result is an empty array then it will use insertMany function to insert the default_Array in the Items Collection 
+// and then redirects to the home route so that the home list refreshes with new document data from the mongodb.
 app.get("/", function(req,res){
     
     Item.find({} ,function(err,foundItems){
@@ -71,7 +78,7 @@ app.get("/", function(req,res){
 
 
 
-
+//creates a new task in the home route list and redirects to home route(/) to update the task list.
 app.post('/',function(req,res){
     const newTask=req.body.task;
     const item = new Item({
@@ -81,6 +88,10 @@ app.post('/',function(req,res){
     res.redirect('/');
 });
 
+
+
+// when this route is called then it checks for the checkboxstatus and if it having any value,
+// then it will delete it from db by using the findByIdAndRemove PaymentMethodChangeEvent.
 app.post('/delete',function(req,res){
     // console.log(req.body.checkboxstatus);
     const deletetask = req.body.checkboxstatus;
@@ -96,24 +107,36 @@ app.post('/delete',function(req,res){
 });
 
 
-app.get('/work',function(req,res){
-    res.render("list",{list_title:"work",taskArray:work_array,dayofweek:day});
+// temporary schema for dynamic routing and dynamic lists
+const listSchema = new mongoose.Schema({
+    name:String,
+    items:[itemsSchema]
+});
+
+const List = new mongoose.model("List",listSchema);
+
+// this is called as a dynamic route as the :id value can be anything.
+app.get('/:id',function(req,res){
+    const customlistname = req.params.id;
+    const list = new List({
+        name:customlistname,
+        items: default_Array
+    });
+    list.save();
 });
 
 
 
-app.post('/work',function(req,res){
-    let work_item = req.body.newTask;
-    work_array.push(work_item);
-    res.redirect('/work');
+List.findOne({name:customlistname},function(err,resultantlist){
+    if(!err){
+        if(!resultantlist){
+            console.log('This name doesnt exist bro!');
+        }
+        else{
+            console.log('this name already exists bro!');
+        }
+    }
 });
-
-
-
-app.get('/about',function(req,res){
-    res.render('about');
-})
-
 
 app.listen(PORT,function(){
     console.log("server started on port "+PORT+" bro!");
